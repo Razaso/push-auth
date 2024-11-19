83f6b5395655c6c7fc02917e43d2347b6fef5d95
rozaso <atheeq@push.org>
2024-11-19 06:19:14 -0800

    fix: Fix CORS issue between the deployed auth service and localhost.

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: '*',
     methods: 'OPTIONS,GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
